6679a9c12c5a31f002028639f861888e6d72f456
Mark Davis <github@nohorse.com>
2025-01-11 16:29:58 -0800

    Refactor provider services and components
    - Renamed `ProviderService` to `OldProviderService` and created a new `ProviderService` with updated methods.
    - Updated dependency injections across various components to use the new service.
    - Simplified data fetching logic in the provider index component, including error handling improvements.
    - Added necessary MudBlazor providers for UI enhancements.
    - Cleaned up unused code and improved overall structure for better maintainability.

--- OpenEugene.Module.LittleHelpBook/Client/Services/ProviderService.cs
+++ OpenEugene.Module.LittleHelpBook/Client/Services/ProviderService.cs
@@ -2,73 +2,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using OpenEugene.Module.LittleHelpBook.ViewModels;
 using Oqtane.Modules;
 using Oqtane.Services;
 using Oqtane.Shared;
+using System.Net;
 
 namespace OpenEugene.Module.LittleHelpBook.Services
 {
-    public class ProviderService : ServiceBase, IService
+    public class ProviderService : ResponseServiceBase, IService
     {
-        public ProviderService(HttpClient http, SiteState siteState) : base(http, siteState) { }
+        public ProviderService(IHttpClientFactory http, SiteState siteState) : base(http, siteState) { }
 
         private string Apiurl => CreateApiUrl("Provider");
 
-        public async Task<List<Models.Provider>> GetProvidersAsync()
+        public async Task<(List<Models.Provider>,HttpStatusCode)> GetProvidersAsync()
         {
-            List<Models.Provider> list = await GetJsonAsync<List<Models.Provider>>($"{Apiurl}");
-            if (list != null)
-            {
-                return list.OrderBy(item => item.Name).ToList();
-            }
-            return null;
+            var url = $"{Apiurl}";
+            (var data, var response) = await GetJsonWithResponseAsync<List<Models.Provider>>(url);
+            return (data, response.StatusCode);      
         }
 
-        public async Task<Models.Provider> GetProviderAsync(int id)
+        public async Task<(Models.Provider, HttpStatusCode)> GetProviderAsync(int id)
         {
-            return await GetJsonAsync<Models.Provider>($"{Apiurl}/{id}");
+            var url = $"{Apiurl}/{id}";
+            (var data, var response) = await GetJsonWithResponseAsync<Models.Provider>(url);
+            return (data, response.StatusCode);        
         }
 
-        public async Task<ProviderViewModel> GetProviderViewModelAsync(int id)
+        public async Task<(Models.Provider, HttpStatusCode)> AddProviderAsync(Models.Provider item)
         {
-            var vm = await GetJsonAsync<ProviderViewModel>($"{Apiurl}/vm/{id}");
-            return vm;
+            var url = $"{Apiurl}";
+            (var data, var response) = await PostJsonWithResponseAsync<Models.Provider>(url,item);
+            return (data, response.StatusCode);        
         }
 
-        public async Task<Models.Provider> AddProviderAsync(Models.Provider item)
+        public async Task<(Models.Provider, HttpStatusCode)> UpdateProviderAsync(Models.Provider item)
         {
-            return await PostJsonAsync<Models.Provider>($"{Apiurl}", item);
+            var url = $"{Apiurl}/{item.ProviderId}";
+            (var data, var response) = await PutJsonWithResponseAsync<Models.Provider>(url,item);
+            return (data, response.StatusCode);        
         }
 
-        public async Task<Models.Provider> UpdateProviderAsync(Models.Provider item)
+        public async Task<HttpStatusCode> DeleteProviderAsync(int id)
         {
-            return await PutJsonAsync<Models.Provider>($"{Apiurl}/{item.ProviderId}", item);
+            var url = $"{Apiurl}/{id}";
+            var response  = await DeleteWithResponseAsync(url);
+            return response.StatusCode;
         }
-
-        public async Task<ProviderViewModel> UpdateProviderAsync(ProviderViewModel item)
-        {
-            return await PutJsonAsync<ProviderViewModel>($"{Apiurl}/vm/{item.ProviderId}", item);
-        }
-
-        public async Task DeleteProviderAsync(int id)
-        {
-            await DeleteAsync($"{Apiurl}/{id}");
-        }
-
-        public async Task<List<ProviderAttributeViewModel>> GetAttributesForProviderAsync(int id) {
-            var vm = await GetJsonAsync<List<ProviderAttributeViewModel>>($"{Apiurl}/ProviderAttributes/{id}");
-            return vm;
-        }
-        public async Task DeleteAttributeAsync(int id)
-        {
-            await DeleteAsync($"{Apiurl}/ProviderAttribute/{id}");
-        }
-        public async Task<Models.ProviderAttribute> AddProviderAttribute(Models.ProviderAttribute item)
-        {
-            item.EnsureIAuditable();
-            return await PostJsonAsync<Models.ProviderAttribute>($"{Apiurl}/ProviderAttribute", item);
-        }
-
     }
 }
