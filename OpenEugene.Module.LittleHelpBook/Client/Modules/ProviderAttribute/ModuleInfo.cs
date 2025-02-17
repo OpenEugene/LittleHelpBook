662c4ad7c68f0a5e1579326a9a20321d523aee1d
Mark Davis <github@nohorse.com>
2025-02-16 17:02:37 -0800

    Update provider details and module info
    - Removed the services field from provider details.
    - Renamed "Provider Attribute" to "Provider Services" in module info.
    - Updated description for clarity.

--- OpenEugene.Module.LittleHelpBook/Client/Modules/ProviderAttribute/ModuleInfo.cs
+++ OpenEugene.Module.LittleHelpBook/Client/Modules/ProviderAttribute/ModuleInfo.cs
@@ -7,8 +7,8 @@ namespace OpenEugene.Module.ProviderAttribute
     {
         public ModuleDefinition ModuleDefinition => new ModuleDefinition
         {
-            Name = "Provider Attribute",
-            Description = "Provider Attribute",
+            Name = "Provider Services",
+            Description = "Provider Services aka Attribute",
             Version = "1.0.0",
             ServerManagerType = "OpenEugene.Module.LittleHelpBook.Manager.LittleHelpBookManager, OpenEugene.Module.LittleHelpBook.Server.Oqtane",
             ReleaseVersions = "1.0.0",
