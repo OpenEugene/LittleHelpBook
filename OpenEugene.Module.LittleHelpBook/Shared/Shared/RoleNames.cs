10eb1e9ca30746c4d4ad0afd9872e8f1136c757b
Mark Davis <github@nohorse.com>
2025-04-09 18:29:47 -0700

    Enables role-based authorization for card addition
    Restricts card addition functionality to users with
    specific roles, enhancing security and access control.
    
    Adds "Hosts Users" role for authorization purposes.

--- OpenEugene.Module.LittleHelpBook/Shared/Shared/RoleNames.cs
+++ OpenEugene.Module.LittleHelpBook/Shared/Shared/RoleNames.cs
@@ -4,5 +4,7 @@ namespace OpenEugene.Module.LittleHelpBook.Shared
 {
     public partial class LhbRoleNames : RoleNames {
         public const string Editors = "Editors";
+        public const string Hosts = "Hosts Users";
+
     }
 }
